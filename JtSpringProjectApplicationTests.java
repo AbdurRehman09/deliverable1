57b1006cf0e4e128e745ebdbd1e5d77d9d3a3b47
Syed-Ibrahim-Shah <102490050+Syed-Ibrahim-Shah@users.noreply.github.com>
2023-12-01 23:41:40 +0500

    Add files via upload
    BugFixed admin HomePage and tested

---
+++ JtSpringProjectApplicationTests.java
@@ -0,0 +1,30 @@
+package com.jtspringproject.JtSpringProject;
+
+import com.jtspringproject.JtSpringProject.controller.AdminController;
+import com.jtspringproject.JtSpringProject.dao.userDao;
+import com.jtspringproject.JtSpringProject.models.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.ui.ConcurrentModel;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class JtSpringProjectApplicationTests {
+	@Test
+	public void testLogout() {
+		AdminController adminController = new AdminController();
+		String loginPage = adminController.returnIndex();
+
+		assertEquals("adminlogin", loginPage);
+	}
+
+	@Test
+	public void testAdminHome() {
+		AdminController adminController = new AdminController();
+		String result = adminController.adminHome(new ConcurrentModel());
+
+		assertEquals("redirect:/admin/login", result);
+	}
+
+}
